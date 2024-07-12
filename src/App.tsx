85554bfd574445265d91a0839fedd214ee5eb04a
Faraz <farazatgithub@gmail.com>
2024-07-12 19:49:06 +0500

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import {useState,useEffect} from "react"
 import "./App.css";
+import User from "./components/User";
 
 function App() {
   //TASK 1 COUNTER
+  const [counter,setCounter] = useState<number>(0)
+  const [users,setUsers] = useState([])
+
+ function increment() {
+   setCounter(prevCount => prevCount + 1)
+ }
+
+ function decrement() {
+  setCounter(prevCount => prevCount - 1)
+}
+
+useEffect(() => {
+   const fetchUsers = async () => {
+      try{
+       const response = await fetch("https://randomuser.me/api") 
+       const reponseUsers = await response.json()
+       setUsers(reponseUsers)
+      }catch(err) {
+        console.log(err)
+      }
+   }
+   fetchUsers()
+},[])
+
+
 
   //TEST 2 FETCHING USER
   // https://randomuser.me/api
@@ -11,10 +38,10 @@ function App() {
 
   return (
     <div className="App">
-      <p>COUNTER: </p>
-      <button>Increment</button>
-      <button>Decrement</button>
-      <hr />
+      <p>COUNTER: {counter} </p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+     <User counter={counter}/>
       
     </div>
   );
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,14 +1,58 @@
+import {useState,useEffect} from "react"
 
-const User = () => {
+type UserType = {
+   firstName:string,
+   lastName:string,
+   city:string,
+   state:string,
+   country:string   
+}
 
+type CounterProp = {
+  counter:number
+}
+
+const User = (prop:CounterProp) => {
+
+  const [paginatedUsers,setPaginatedUsers] = useState<UserType[] | []>([])
+
+
+  useEffect(() => {
+    const fetchPagintedUsers = async () => {
+      try{
+       const response = await fetch(`https://randomuser.me/api?page=${prop.counter}&result=${prop.counter}`) 
+       const reponseUsers = await response.json()
+       const modfifiedUsers = reponseUsers.results.map((result:any) => {
+           return {
+             firstName:result?.name?.first,
+             lastName:result?.name?.last,
+             city:result?.city,
+             state:result?.state,
+             country:result?.country,
+             street:result?.location?.street?.name
+           }
+       })
+       setPaginatedUsers(modfifiedUsers)
+      }catch(err) {
+        console.log(err)
+      }
+   }
+   fetchPagintedUsers()
+  }, [prop.counter])
+  
 
   return (
     <div>
-      <p>First Name: </p>
-      <p>Last Name: </p>
-      <p>City: </p>
-      <p>State: </p>
-      <p>Country: </p>
+      {paginatedUsers?.map((user:any) => {
+        return <div> 
+           <p>FirstName: {user?.firstName}</p>
+           <p>LastName: {user?.lastName}</p>
+           <p>city: {user?.city}</p>
+           <p>state: {user?.state}</p>
+           <p>country: {user?.country}</p>
+           <p>country: {user?.street}</p>
+        </div>
+      })}
     </div>
   );
 };
